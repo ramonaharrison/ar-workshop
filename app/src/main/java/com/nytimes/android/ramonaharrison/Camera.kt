6773e7aa77d9076851f3d75804d93fbc494db552
Ramona Harrison <ramona.harrison@nytimes.com>
2019-03-21 18:21:10 -0400

    Add Camera helper class

---
+++ app/src/main/java/com/nytimes/android/ramonaharrison/Camera.kt
@@ -0,0 +1,105 @@
+package com.nytimes.android.ramonaharrison
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Environment
+import android.os.Handler
+import android.os.HandlerThread
+import android.view.PixelCopy
+import android.view.SurfaceView
+import android.widget.Toast
+import androidx.core.content.FileProvider
+import com.google.android.material.snackbar.Snackbar
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+interface CapturableView {
+    fun getViewToCapture(): SurfaceView
+}
+
+class Camera(private val activity: Activity, private val fragment: CapturableView) {
+
+    fun snap() {
+        val filename = generateFilename()
+        val view = fragment.getViewToCapture()
+
+        // Create a bitmap the size of the scene view.
+        val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+
+        // Create a handler thread to offload the processing of the image.
+        val handlerThread = HandlerThread("PixelCopier")
+        handlerThread.start()
+
+        // Make the request to copy.
+        PixelCopy.request(view, bitmap, { copyResult : Int ->
+            if (copyResult == PixelCopy.SUCCESS) {
+                try {
+                    saveBitmapToDisk(bitmap, filename)
+                } catch (e: IOException) {
+                    val toast = Toast.makeText(
+                        activity, e.toString(),
+                        Toast.LENGTH_LONG
+                    )
+                    toast.show()
+                    return@request
+                }
+
+                val snackbar = Snackbar.make(
+                    activity.findViewById(android.R.id.content),
+                    "Photo saved", Snackbar.LENGTH_LONG
+                )
+                snackbar.setAction("Open in Photos") { v ->
+                    val photoFile = File(filename)
+                    val photoURI = FileProvider.getUriForFile(
+                        activity,
+                        activity.packageName + ".ar.codelab.name.provider",
+                        photoFile
+                    )
+                    val intent = Intent(Intent.ACTION_VIEW, photoURI)
+                    intent.setDataAndType(photoURI, "image/*")
+                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                    activity.startActivity(intent)
+
+                }
+                snackbar.show()
+            } else {
+                val toast = Toast.makeText(
+                    activity, "Failed to copyPixels: $copyResult", Toast.LENGTH_LONG
+                )
+                toast.show()
+            }
+            handlerThread.quitSafely()
+        }, Handler(handlerThread.looper))
+    }
+
+    @Throws(IOException::class)
+    private fun saveBitmapToDisk(bitmap: Bitmap, filename: String) {
+        val out = File(filename)
+        if (!out.parentFile.exists()) {
+            out.parentFile.mkdirs()
+        }
+        try {
+            FileOutputStream(filename).use { outputStream ->
+                ByteArrayOutputStream().use { outputData ->
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputData)
+                    outputData.writeTo(outputStream)
+                    outputStream.flush()
+                    outputStream.close()
+                }
+            }
+        } catch (ex: IOException) {
+            throw IOException("Failed to save bitmap to disk", ex)
+        }
+    }
+
+    private fun generateFilename(): String {
+        val date = SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault()).format(Date())
+        return "${Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)}${File.separator}Sceneform/${date}_screenshot.jpg"
+    }
+}
