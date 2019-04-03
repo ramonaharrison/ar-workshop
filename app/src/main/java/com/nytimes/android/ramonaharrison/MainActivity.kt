0d800cfeb3a16da51370475bf3795b18caa799bc
Ramona Harrison <ramona.harrison@nytimes.com>
2019-04-03 16:12:01 +0200

    finish part i

--- app/src/main/java/com/nytimes/android/ramonaharrison/MainActivity.kt
+++ app/src/main/java/com/nytimes/android/ramonaharrison/MainActivity.kt
@@ -1,27 +1,43 @@
 package com.nytimes.android.ramonaharrison
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.snackbar.Snackbar
+import com.google.ar.core.Anchor
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.MaterialFactory
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.Renderable
+import com.google.ar.sceneform.rendering.ShapeFactory
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
+import com.nytimes.android.ramonaharrison.helpers.CameraHelper
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     private val galleryAdapter = GalleryAdapter()
     private lateinit var galleryRecyclerview: RecyclerView
 
+    private lateinit var arFragment: ArFragment
+    private lateinit var camera: CameraHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
-        }
 
         galleryRecyclerview = findViewById<RecyclerView>(R.id.gallery_recyclerview).apply {
             layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
@@ -29,6 +45,47 @@ class MainActivity : AppCompatActivity() {
             setHasFixedSize(true)
         }
 
+        arFragment = fragment as ArFragment
+        arFragment.setOnTapArPlaneListener { hitResult, plane, motionEvent ->
+            val anchor = hitResult.createAnchor()
+            val uri = galleryAdapter.getSelected().getUri()
+            placeObject(anchor, uri)
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 0)
+        }
+
+        camera = CameraHelper(this, arFragment.arSceneView)
+        fab.setOnClickListener { camera.snap() }
+    }
+
+    private fun addSphere(color: Int, anchor: Anchor, radius: Float, centerX : Float, centerY: Float, centerZ : Float) {
+        MaterialFactory.makeOpaqueWithColor(this, com.google.ar.sceneform.rendering.Color(color))
+            .thenAccept { material ->
+                val shape = ShapeFactory.makeSphere(radius, Vector3(centerX, centerY, centerZ), material)
+                addNodeToScene(anchor, shape)
+            }
+    }
+
+    private fun addNodeToScene(anchor: Anchor, renderable: Renderable) {
+        val anchorNode = AnchorNode(anchor)
+        val node = TransformableNode(arFragment.transformationSystem)
+        node.renderable = renderable
+        node.setParent(anchorNode)
+        arFragment.arSceneView.scene.addChild(anchorNode)
+    }
+
+    private fun placeObject(anchor: Anchor, model: Uri) {
+        ModelRenderable.builder()
+            .setSource(fragment.context, model)
+            .build()
+            .thenAccept { renderable -> addNodeToScene(anchor, renderable) }
+            .exceptionally { throwable ->
+                Log.e("MainActivity", throwable.message)
+                Toast.makeText(this@MainActivity, "Something went wrong!", Toast.LENGTH_SHORT).show()
+                null
+            }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
